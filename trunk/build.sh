f0b94b43c4820276ff9cd014543c9a6e84e9418c
sancome <sancome@openwrt01.(none)>
2014-01-31 12:29:16 +0800

    update trunk files

--- trunk/build.sh
+++ trunk/build.sh
@@ -5,100 +5,65 @@
 cd ~/openwrt
 
 svn checkout svn://svn.openwrt.org/openwrt/trunk trunk
-git clone git://github.com/sancome/my_openwrt_mod.git
+git clone git://github.com/sancome/zd_openwrt_mod.git
 #git clone git://github.com/binux/yaaw.git
 #git clone git://github.com/ziahamza/webui-aria2.git
 
 cp ./trunk/feeds.conf.default ./trunk/feeds.conf
 #echo "src-git exopenwrt https://github.com/black-roland/exOpenWrt.git" >> ./trunk/feeds.conf
 #echo "src-git mwan git://github.com/Adze1502/mwan.git" >> ./trunk/feeds.conf
+
 ./trunk/scripts/feeds update -a
 ./trunk/scripts/feeds install -a
 
 #RA MOD
-#patch
-mkdir ./trunk/feeds/packages/net/aria2/patches
-cp -rf ./my_openwrt_mod/patch/aria2/*.* ./trunk/feeds/packages/net/aria2/patches
-cp -rf ./my_openwrt_mod/patch/uClibc/*.* ./trunk/toolchain/uClibc/patches-0.9.33.2
-cp -rf ./my_openwrt_mod/patch/busybox/*.* ./trunk/package/utils/busybox/patches
-
-#package base-files
-cp -rf ./my_openwrt_mod/package/base-files/sysctl.conf.diff ./trunk/package/base-files/files/etc/
-patch -p0 ./trunk/package/base-files/files/etc/sysctl.conf < ./trunk/package/base-files/files/etc/sysctl.conf.diff
-rm ./trunk/package/base-files/files/etc/sysctl.conf.diff
-cp -rf ./my_openwrt_mod/package/base-files/sysupgrade.conf ./trunk/package/base-files/files/etc/
-cp -rf ./my_openwrt_mod/package/base-files/banner ./trunk/package/base-files/files/etc/
-cp -rf ./my_openwrt_mod/package/base-files/profile ./trunk/package/base-files/files/etc/
-cp -rf ./my_openwrt_mod/package/base-files/tsocks.conf ./trunk/package/base-files/files/etc/
-cp -rf ./my_openwrt_mod/package/base-files/rc.button ./trunk/package/base-files/files/etc/
-cp -rf ./my_openwrt_mod/package/base-files/dnsmasq.d ./trunk/package/base-files/files/etc/
-cp -rf ./my_openwrt_mod/package/base-files/ipset ./trunk/package/base-files/files/etc/
-#cp -rf ./my_openwrt_mod/package/base-files/config/wireless ./trunk/package/base-files/files/etc/config/
-cp -rf ./my_openwrt_mod/package/base-files/sbin/aria2 ./trunk/package/base-files/files/sbin/
-##for tl841v7
-#cp -rf ./my_openwrt_mod/package/base-files/hotplug.d ./trunk/package/base-files/files/etc/
-
 #package files
-#dnsmasq
-cp -rf ./my_openwrt_mod/package/dnsmasq/*.* ./trunk/package/network/services/dnsmasq/files/
-#samba
-cp -rf ./my_openwrt_mod/package/samba/*.* ./trunk/package/network/services/samba36/files
-#ubox
-cp -rf ./my_openwrt_mod/package/ubox ./trunk/package/system/
-patch -p0 ./trunk/package/system/ubox/Makefile < ./trunk/package/system/ubox/Makefile.diff
-rm ./trunk/package/system/ubox/Makefile.diff
 #openssl
-cp -rf ./my_openwrt_mod/package/openssl ./trunk/package/libs/
+cp -rf ./zd_openwrt_mod/package/openssl ./trunk/package/libs/
 patch -p0 ./trunk/package/libs/openssl/Makefile < ./trunk/package/libs/openssl/Makefile.diff
 rm ./trunk/package/libs/openssl/Makefile.diff
 
 #package install
 #exfat-nofuse
-cp -rf ./my_openwrt_mod/package/exfat-nofuse ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/exfat-nofuse ./trunk/package/
 #redsocks
-cp -rf ./my_openwrt_mod/package/redsocks2/ ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/redsocks2/ ./trunk/package/
 #shadowsocks-libev
-cp -rf ./my_openwrt_mod/package/shadowsocks-libev ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/shadowsocks-libev ./trunk/package/
 #添加tsocks 远程dns支持
-cp -rf ./my_openwrt_mod/package/tsocks ./trunk/feeds/packages/net/
+cp -rf ./zd_openwrt_mod/package/tsocks ./trunk/feeds/packages/net/
 #dnscrypt-proxy
-cp -rf ./my_openwrt_mod/package/dnscrypt-proxy ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/dnscrypt-proxy ./trunk/package/
 #libsodium
-cp -rf ./my_openwrt_mod/package/libsodium ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/libsodium ./trunk/package/
 #nodejs
-cp -rf ./my_openwrt_mod/package/nodejs ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/nodejs ./trunk/package/
 #cpulimit-ng
-cp -rf ./my_openwrt_mod/package/cpulimit-ng ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/cpulimit-ng ./trunk/package/
 #gevent
-cp -rf ./my_openwrt_mod/package/gevent ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/gevent ./trunk/package/
 #greenlet
-cp -rf ./my_openwrt_mod/package/greenlet ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/greenlet ./trunk/package/
 #shairport-new
-cp -rf ./my_openwrt_mod/package/shairport-new ./trunk/package/
+cp -rf ./zd_openwrt_mod/package/shairport-new ./trunk/package/
+#gmediarender
+cp -rf ./zd_openwrt_mod/package/gmediarender ./trunk/package/
 
 #luci
-cp -rf ./my_openwrt_mod/luci ./trunk/feeds/
+cp -rf ./zd_openwrt_mod/luci ./trunk/feeds/
 patch -p0 ./trunk/feeds/luci/contrib/package/luci/Makefile < ./trunk/feeds/luci/contrib/package/luci/Makefile.diff
 rm ./trunk/feeds/luci/contrib/package/luci/Makefile.diff
-#luci vlan 96
-patch -p0 ./trunk/feeds/luci/modules/admin-full/luasrc/model/cbi/admin_network/vlan.lua < ./trunk/feeds/luci/modules/admin-full/luasrc/model/cbi/admin_network/vlan.lua.diff
-rm ./trunk/feeds/luci/modules/admin-full/luasrc/model/cbi/admin_network/vlan.lua.diff
-
-#switch vlan 96
-cp -rf ./my_openwrt_mod/target ./trunk/
-patch -p0 ./trunk/target/linux/generic/files/drivers/net/phy/b53/b53_common.c < ./trunk/target/linux/generic/files/drivers/net/phy/b53/b53_common.c.diff
-rm ./trunk/target/linux/generic/files/drivers/net/phy/b53/b53_common.c.diff
 
 #files
-cp -rf ./my_openwrt_mod/files ./trunk/
+cp -rf ./zd_openwrt_mod/files ./trunk/
 
 #add yaaw
 #cp -rf ./yaaw ./trunk/feeds/luci/modules/admin-core/root/www/
 #add webui-aria2
 #cp -rf ./webui-aria2 ./trunk/feeds/luci/modules/admin-core/root/www/
 
-#delete my_openwrt_mod
-rm -rf ./my_openwrt_mod
+#delete zd_openwrt_mod
+rm -rf ./zd_openwrt_mod
 
 #save dl files to dl-trunk
 if [ ! -d dl-trunk ]; then
